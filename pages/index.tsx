3f2121bf5994a01285c95bc90b736407f07dc058
leitongda <ebian123@qq.com>
2021-05-07 23:29:40 +0800

    '更正类型'

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ export async function getStaticProps() {
   }
 }
 
-export default function Home({ allPostsData }) {
+export default function Home({ allPostsData }: { allPostsData: any }) {
   const { data, error } = useSWR('/api/hello', fetch)
 
   return (
@@ -27,7 +27,7 @@ export default function Home({ allPostsData }) {
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
+          {allPostsData.map(({ id, date, title }: { id: string, date: string, title: string }) => (
             <li className={utilStyles.listItem} key={id}>
               <Link href={`/posts/${id}`}>
                 <a>{title}</a>
