51a5c893639070eef5852b1ffdceefa16a09fe41
leitongda <ebian123@qq.com>
2021-05-07 23:04:02 +0800

    '官网'

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,53 @@
+import { useState } from 'react'
 import useSWR from 'swr'
 import Link from 'next/link'
 import Head from 'next/head'
-import Date from '../components/date'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
+import Image from 'next/image'
+import siteStyles from '../styles/site.module.scss'
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
-}
-
-export default function Home({ allPostsData }) {
-  const { data, error } = useSWR('/api/hello', fetch)
+export default function Home() {
+  const [active, setActive] = useState('')
 
   return (
-    <Layout home>
+    <>
       <Head>
-        <title>{siteTitle}</title>
+        <title>四川比盟科技有限公司</title>
       </Head>
-      <section className={utilStyles.headingMd}>…</section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
+      <header className={siteStyles.header}>
+        <Link href="/">
+          <Image
+            src="/images/logo.png"
+            alt="比盟科技"
+            title="四川比盟科技有限公司"
+            width={200}
+            height={40}
+          />
+        </Link>
+        <ul className={siteStyles.menu}>
+          <ol onClick={() => setActive('')} className={active === '' ? siteStyles.active : ''}>
+            <Link href="/"><a>首页</a></Link>
+          </ol>
+          <ol onClick={() => setActive('product')} className={active === 'product' ? siteStyles.active : ''}>
+            <Link href="#product"><a>产品及服务</a></Link>
+          </ol>
+          <ol onClick={() => setActive('example')} className={active === 'example' ? siteStyles.active : ''}>
+            <Link href="#example"><a>重点案例</a></Link>
+          </ol>
+          <ol onClick={() => setActive('technology')} className={active === 'technology' ? siteStyles.active : ''}>
+            <Link href="/technology"><a>技术支持</a></Link>
+          </ol>
+          <ol onClick={() => setActive('about')} className={active === 'about' ? siteStyles.active : ''}>
+            <Link href="#about"><a>关于比盟</a></Link>
+          </ol>
         </ul>
-      </section>
-    </Layout>
+      </header>
+
+      <Image
+        src="/images/banner1.png"
+        alt="轮播图"
+        width="1920"
+        height="820"
+      />
+    </>
   )
 }
